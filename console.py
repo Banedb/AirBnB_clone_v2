eb2c1da9748ac86f67823f16e8845ffd0d3d73bb
Bane <marcytornad@gmail.com>
2025-07-30 16:17:28 +0000

    3-5

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
-    prompt = '(hbnb) ' # if sys.__stdin__.isatty() else ''
+    prompt = '(hbnb) '  # if sys.__stdin__.isatty() else ''
 
     classes = {
                'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -327,6 +327,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,21 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {
+                k: v for k, v in FileStorage.__objects.items()
+                if isinstance(v, cls)
+            }
         return FileStorage.__objects
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if available."""
+        if obj:
+            FileStorage.__objects = {
+                k: v for k, v in FileStorage.__objects.items() if v != obj
+            }
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +57,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
