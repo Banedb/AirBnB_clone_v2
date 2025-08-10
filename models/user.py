e688b8352434165d20799ec1281589c1387756b2
Bane <marcytornad@gmail.com>
2025-08-09 18:31:13 -0700

    tests

--- models/user.py
+++ models/user.py
@@ -8,10 +8,10 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column(String(128), nullable=False, default="")
+    password = Column(String(128), nullable=False, default="")
+    first_name = Column(String(128), default="")
+    last_name = Column(String(128), default="")
     places = relationship(
         "Place", backref="user", cascade="all, delete-orphan"
     )
