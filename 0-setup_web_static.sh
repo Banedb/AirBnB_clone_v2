57d5b459e1463b41f980851d2469324bbe51213e
Bane <marcytornad@gmail.com>
2025-08-14 15:13:23 +0000

    reload

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Sets up the web servers for the deployment of `web_static`.
+# Sets up the web server for the deployment of `web_static`.
 if ! command -v nginx &> /dev/null; then
     apt update
     apt install nginx -y
@@ -7,11 +7,11 @@ if ! command -v nginx &> /dev/null; then
     # sudo systemctl enable nginx
 fi
 mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "<html><body>Hello from ALX</body></html>" > /data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\nALX\n  </body>\n</html>" > /data/web_static/releases/test/index.html
 # echo "<html><body>Hello from ALX</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu:ubuntu /data/ -R
 if ! grep -q "location /hbnb_static/" /etc/nginx/sites-available/default; then
 sed -i '/server_name _;/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}' /etc/nginx/sites-available/default
 fi
-service nginx start
+service nginx reload
