81c0b538fef7067538fffcea870adbecdd856cba
Bane <marcytornad@gmail.com>
2025-08-15 13:50:00 +0000

    100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""100-clean_web_static module"""
+from fabric.api import env, local, run
+
+env.hosts = ["172.18.0.3", "172.18.0.4"]
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives.
+    Args:
+        number(int): Number of archives to keep.
+    """
+    keep = 2 if number == 0 else int(number) + 1
+    local(
+        f'ls -1t versions | grep "^web_static_" | tail -n +{keep} | '
+        'xargs -I ar rm -f versions/ar'
+    )
+    run(
+        'ls -1t /data/web_static/releases | grep "^web_static_" | '
+        f'tail -n +{keep} | xargs -I ar rm -rf /data/web_static/releases/ar'
+    )
