408b1b44facd2920dff5639979b8674164b48dc1
Bane <marcytornad@gmail.com>
2025-09-03 10:11:07 +0000

    cleanup

--- models/state.py
+++ models/state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from datetime import datetime
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -10,9 +9,6 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    id = Column(String(60), primary_key=True)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     name = Column(String(128), nullable=False, default="")
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
